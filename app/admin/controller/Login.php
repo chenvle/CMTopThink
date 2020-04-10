84ba8850b61ef2d92e92650a6e1879b52d63b894
chenvle <chenvle@cmtime.cn>
2020-04-11 01:41:31 +0800

    登陆页面还原

--- app/admin/controller/Login.php
+++ app/admin/controller/Login.php
@@ -14,11 +14,6 @@ class Login extends BaseController
     protected $middleware = [Auth::class];
     public function index(Request $request)
     {
-        $type = $request->param('type');
-        if($type == 'admin'){
-            return view('index',['Message'=>get_message(),'is_admin'=>'','title'=>'CMtime晨明网络']);
-        }else{
-            return view('index',['Message'=>get_message(),'is_admin'=>'user_login','title'=>'外贸管理系统']);
-        }
+        return view('index',['Message'=>get_message(),'is_admin'=>'','title'=>'CMtime晨明网络']);
     }
 }
