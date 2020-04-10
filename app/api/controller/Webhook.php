324cbc5bab2bc6ef8a43e2316966da983ae5fcb1
chenvle <chenvle@cmtime.cn>
2020-04-11 01:51:17 +0800

    登陆页面还原

--- app/api/controller/Webhook.php
+++ app/api/controller/Webhook.php
@@ -9,6 +9,16 @@ class Webhook extends BaseController
 {
     public function pull()
     {
-        return 'ok';
+        error_reporting ( E_ALL );
+
+        $dir = '/www/wwwroot/CMTopThink/';//该目录为git检出目录
+
+        $handle = popen('cd '.$dir.' && git pull 2>&1','r');
+
+        $read = stream_get_contents($handle);
+
+        printf($read);
+
+        pclose($handle);
     }
 }
